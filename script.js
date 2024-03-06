9d0cdddf20c76a2b542b929ddec1382ddaff5330
Junior Reis <gilmaikjunior@gmail.com>
2024-03-06 00:00:08 -0300

    HTML e CSS pronto

---
+++ script.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById("Nome").value
+const sobrenome = document.getElementById("Sobrenome").value
+const idade = Number(document.getElementById("Idade").value)
+const email = document.getElementById("Email").value
+const sexo = document.getElementById("Sexo").value
+const senha = Number(document.getElementById("Senha").value)
+const botao = document.getElementsByClassName("Enviar")
